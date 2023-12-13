db09f2e83d57dfa9a64a8fb3066c77fe2628e725
MiguelAngelIngaMorales <152421813+MiguelAngelIngaMorales@users.noreply.github.com>
2023-12-13 00:34:04 -0500

    Update nmatricula.java

--- analisis y diseño proyecto final/analisis y diseño proyecto final/Matricula_Asistencia/src/Controladores/nmatricula.java
+++ analisis y diseño proyecto final/analisis y diseño proyecto final/Matricula_Asistencia/src/Controladores/nmatricula.java
@@ -6,3 +6,22 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+
+public class nmatricula{
+  private conexion mysql = new conexion();
+  private Connection cn = mysql.Connectar();
+  private String sql;
+
+public DefaultTableModel mostrar (String buscar){
+  DefaultTableModel modelo;
+  String [] titulos = {"Matricula","Codigo","Dni","Nombres","Apellidos","Grado","Nivel","Periodo","Estado","N"};
+  String [] registros = new String [10];
+  modelo = new DefaultTableModel(null, titulos);
+
+  sql = "SELECT matricula.idmatricula, estudiante.idEstudiante, estudiante.identificacion, estudiante.Nombres,
+    estudainte.Apellidos, grado.nombre AS nombre, grado.nivel, matricula.periodo, matricula.estado, grado.idgrado FROM
+    estudiante INNER JOIN matricula ON estudiante.idEstudiante = matricula.idestudiante INNER JOIN grado ON matricula.idgrado = 
+    grado.idgrado WHERE estudiante.apellidos LIKE ?";
+
+  try {
+    PreparedStatement 
